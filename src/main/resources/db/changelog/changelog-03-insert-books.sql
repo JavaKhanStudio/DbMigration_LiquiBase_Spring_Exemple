03038523aa9385e92bae3c812318cbac1a758d53
Simon Bédard <JavaKhanStudio@Gmail.com>
2024-02-15 08:57:58 +0100

    Commit de mise en place

---
+++ src/main/resources/db/changelog/changelog-03-insert-books.sql
@@ -0,0 +1,11 @@
+-- insert-books.sql
+INSERT INTO books (title, author, isbn, price, publish_date) VALUES ('1984', 'George Orwell', '9780451524935', 9.99, '1949-06-08');
+INSERT INTO books (title, author, isbn, price, publish_date) VALUES ('To Kill a Mockingbird', 'Harper Lee', '9780061120084', 14.99, '1960-07-11');
+INSERT INTO books (title, author, isbn, price, publish_date) VALUES ('The Great Gatsby', 'F. Scott Fitzgerald', '9780743273565', 13.99, '1925-04-10');
+INSERT INTO books (title, author, isbn, price, publish_date) VALUES ('One Hundred Years of Solitude', 'Gabriel García Márquez', '9780060883287', 18.99, '1967-06-05');
+INSERT INTO books (title, author, isbn, price, publish_date) VALUES ('A Passage to India', 'E.M. Forster', '9780156711425', 12.99, '1924-06-04');
+INSERT INTO books (title, author, isbn, price, publish_date) VALUES ('Invisible Man', 'Ralph Ellison', '9780679732761', 15.99, '1952-04-14');
+INSERT INTO books (title, author, isbn, price, publish_date) VALUES ('Don Quixote', 'Miguel de Cervantes', '9780060934347', 14.95, '1605-01-16');
+INSERT INTO books (title, author, isbn, price, publish_date) VALUES ('Beloved', 'Toni Morrison', '9781400033416', 16.00, '1987-09-16');
+INSERT INTO books (title, author, isbn, price, publish_date) VALUES ('Mrs. Dalloway', 'Virginia Woolf', '9780156628709', 13.95, '1925-05-14');
+INSERT INTO books (title, author, isbn, price, publish_date) VALUES ('Things Fall Apart', 'Chinua Achebe', '9780385474542', 11.95, '1958-06-17');
